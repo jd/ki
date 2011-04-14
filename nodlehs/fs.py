5358b5b75c14464697893c536d32eb839cc14e42
Julien Danjou <julien@danjou.info>
2011-04-14 15:52:35 +0200

    Add encoding information
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- nodlehs/fs.py
+++ nodlehs/fs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # nodlehs.fs -- Fuse based file system
 #
--- nodlehs/storage.py
+++ nodlehs/storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # nodlehs.storage -- Git based file system storage
 #
