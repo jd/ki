b4d464fb931d2cc0bfedc2f9957c603318338518
Julien Danjou <julien@danjou.info>
2011-04-25 11:21:45 +0200

    utils: replace OrderedSet implementation
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- t/TestUtils.py
+++ t/TestUtils.py
@@ -62,7 +62,7 @@ class TestUtils(unittest.TestCase):
         s.add("hi")
         s.update(OrderedSet([1, 3, 8]))
         self.assert_(s[5] is 8)
-        self.assert_(s.keys() == [ 1, 3, 4, 5, 'hi', 8 ])
+        self.assert_(s == [ 1, 3, 4, 5, 'hi', 8 ])
 
 if __name__ == '__main__':
     unittest.main()
