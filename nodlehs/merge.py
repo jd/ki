8988077e2cc15de1ef0012a1bbdf331f4f08eda3
Julien Danjou <julien@danjou.info>
2011-04-25 19:08:34 +0200

    merge: add
    Signed-off-by: Julien Danjou <julien@danjou.info>

---
+++ nodlehs/merge.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# nodlehs.merge -- Merge files
+#
+#    Copyright © 2011  Julien Danjou <julien@danjou.info>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import subprocess
+import tempfile
+import os
+
+class MergeConflictError(Exception):
+    pass
+
+def merge(current, base, other):
+    """Use git-merge-file to merge content."""
+    file_current = tempfile.mktemp()
+    file_base = tempfile.mktemp()
+    file_other = tempfile.mktemp()
+    try:
+        with file(file_current, "wb") as f:
+            f.write(current)
+        with file(file_base, "wb") as f:
+            f.write(base)
+        with file(file_other, "wb") as f:
+            f.write(other)
+        git_merge_file = subprocess.Popen([ "git", "merge-file", "--stdout",
+                                            file_current, file_base, file_other ],
+                                          stdout=subprocess.PIPE)
+        git_merge_file.wait()
+    finally:
+        try:
+            os.unlink(file_current)
+        except:
+            pass
+        try:
+            os.unlink(file_base)
+        except:
+            pass
+        try:
+            os.unlink(file_other)
+        except:
+            pass
+
+    if git_merge_file.returncode != 0:
+        raise MergeConflictError("%d conflict(s)" % git_merge_file.returncode)
+
+    return git_merge_file.communicate()[0]
+
+
+
---
+++ t/TestMerge.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import unittest
+from nodlehs.merge import *
+
+class TestMerge(unittest.TestCase):
+
+    def test_merge_conflict(self):
+        try:
+            merge("hello world", "hello there", "hello you")
+        except MergeConflictError:
+            self.assert_(True)
+        else:
+            self.assert_(False)
+
+    def test_merge_ok(self):
+        self.assert_(merge("hello\nyou", "hello", "hello") == "hello\nyou")
+        self.assert_(merge("hello\nyou", "hello", "hello\nyou") == "hello\nyou")
+
+if __name__ == '__main__':
+    unittest.main()
